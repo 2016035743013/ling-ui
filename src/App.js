35bf065da99517f77a6f7c9bdc63236ada16dc95
zhangshengbin <zhangshengbin@do1.com.cn>
2020-09-21 21:38:42 +0800

    '按钮组件'

--- src/App.js
+++ src/App.js
@@ -4,20 +4,26 @@
 import React from 'react';
 import { Button } from './components/index'
 class App extends React.Component {
+  handleClick = () => {
+    console.log('这是触发了点击事件')
+  }
   render () {
     return (
       <div className="App">
         <Button> 我是按钮 </Button>
         <Button type='primary'> 我是按钮 </Button>
-        <Button type='ghost'> 我是按钮 </Button>
         <Button type='text'> 我是按钮 </Button>
         <Button type='link'> 我是按钮 </Button>
-        <Button  type='primary' shape='circle' icon={ <i className="iconfont">&#xe608;</i> }></Button>
-        <Button  type='ghost' shape='circle' icon={ <i className="iconfont">&#xe608;</i> }></Button>
-        <Button  shape='circle' icon={ <i className="iconfont">&#xe608;</i> }></Button>
-        <Button type='primary' shape='round' icon={ <i className="iconfont">&#xe608;</i> }> 我是搜索按钮 </Button>
-        <Button  shape='round' icon={ <i className="iconfont">&#xe608;</i> } disabled={true}> 我是搜索按钮 </Button>
-        <Button  type='danger' loading={true}> 我是搜索按钮 </Button>
+        <Button type='primary' shape='circle' icon={<i className="iconfont">&#xe608;</i>}></Button>
+        <Button shape='circle' icon={<i className="iconfont">&#xe608;</i>}></Button>
+        <Button shape='circle' icon={<i className="iconfont">&#xe608;</i>}></Button>
+        <Button type='primary' shape='round' icon={<i className="iconfont">&#xe608;</i>}> 我是搜索按钮 </Button>
+        <Button shape='round' icon={<i className="iconfont">&#xe608;</i>} disabled={true}> 我是搜索按钮 </Button>
+        <Button type='danger' loading={true}> 我是搜索按钮 </Button>
+        <Button type='danger' loading={true} size='large'> 我是搜索按钮 </Button>
+        <Button type='danger' loading={true} size='small'> 我是搜索按钮 </Button>
+        <Button type='danger' ghost={true}> 我是搜索按钮 </Button>
+        <Button type='danger' ghost={true} onClick={this.handleClick}> 我是点击按钮 </Button>
       </div>
     );
   }
--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -20,20 +20,26 @@ class Button extends React.Component {
     type: PropTypes.string,
     onClick: PropTypes.func
   }
-  render () {
-    const { type, children, shape, icon, disabled, loading } = this.props 
+  handleClick = (e) => {
+    this.props.onClick(e)
+  }
+  setBtnClass = () => {
+    const { type, shape, disabled, size, ghost } = this.props 
     let buttonType = prefix
     switch (type) {
       case 'primary': buttonType += 'primary'; break;
-      case 'ghost': buttonType += 'ghost'; break;
       case 'dashed': buttonType += 'dashed'; break;
       case 'danger': buttonType += 'danger'; break;
       case 'link': buttonType += 'link'; break;
       case 'text': buttonType += 'text'; break;
       default:  buttonType += 'default'; break;
     }
+    return [ disabled ? '' : buttonType, shape ? prefix + shape : '', disabled ? 'ling-disabled' : '', size ? prefix + size : '', ghost ? 'ling-ghost' : ''].join(' ').trim()
+  }
+  render () {
+    const { children,icon, loading } = this.props 
     return (
-      <div className={[disabled ? '' : buttonType, shape ? prefix + shape : '', disabled ? 'ling-disabled' : ''].join(' ')}>
+      <div className={this.setBtnClass()} onClick={this.handleClick}>
         {
           loading ? <span className='ling-loading'> <i className='iconfont'>&#xe6cd;</i> </span> : ''
         }
