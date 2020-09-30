c8531b313d63cd4390920d08481ba356bb9c90de
2016035743013 <2199397354@qq.com>
2020-09-30 18:21:13 +0800

    'radiogroup组件初步完成'

--- src/App.js
+++ src/App.js
@@ -2,20 +2,34 @@
 // 组件库参考文章  https://juejin.im/post/6844903749912100871#heading-6
 
 import React from 'react';
-import { Radio } from './components/index'
+import { Radio, RadioGroup } from './components/index'
 import './App.scss'
-const MyContext = React.createContext()
 class App extends React.Component {
-  handleClick = () => {
-    console.log('这是触发了点击事件')
+  constructor(props) {
+    super(props) 
+    this.state = {
+      value: '0',
+      isSelect: false
+    }
+  }
+  handleChange = (value) => {
+    this.setState(() => {
+      return {
+        value
+      }
+    })
   }
   render () {
+    const { value, isSelect } = this.state
     return (
-      <MyContext.Provider value={{test: 'test'}}>
-        <div className="App">
-          <Radio>test</Radio>
-        </div>
-      </MyContext.Provider>
+      <div className="App">
+        <RadioGroup value={value} onChange={this.handleChange}>
+          <Radio style={{display: 'block'}} value='0'>男性</Radio>
+          <Radio value='1'>女性</Radio>
+        </RadioGroup>
+        <Radio style={{display: 'block'}} value='1' checked={true} disabled={true}>女性</Radio>
+
+      </div>
     );
   }
 }
