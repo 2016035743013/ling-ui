ec5ed9cdc9044e12343c1f22d24a02c2ab1587e8
2016035743013 <2199397354@qq.com>
2020-10-13 21:34:33 +0800

    '给slider添加step参数'

--- src/components/slider/index.jsx
+++ src/components/slider/index.jsx
@@ -13,9 +13,10 @@ export default class Slider extends React.Component {
   handleMouseDown = (e) => {
     e.persist()
     let containerWidth = this.container.current.offsetWidth
-    const { min, max, step } = this.props
-    const minValue = min / 100 * containerWidth
-    const maxValue = (max / 100 > 1 ? 1 : max / 100) * containerWidth
+    const { step, disabled } = this.props
+    if (disabled) {
+      return
+    }
     let handlemousemove = (e) => {
       let left = e.clientX - this.container.current.offsetLeft
       console.log(e.clientX || e.pageX, this.container.current.offsetLeft, left)
@@ -23,6 +24,12 @@ export default class Slider extends React.Component {
         left = -6
       } else if (left >= containerWidth - 6) {
         left = containerWidth - 6
+      }
+      if (this.state.left > left) {
+        console.log('向左')
+        left = this.state.left - Math.floor( (this.state.left - left) / step ) * step
+      } else if (this.state.left < left) {
+        left = Math.floor( (left - this.state.left) / step ) * step + this.state.left
       }
       this.setState({
         left,
@@ -39,13 +46,13 @@ export default class Slider extends React.Component {
   }
   render () {
     const { left, isMove } = this.state
-    const { min, max, step } = this.props
+    const { min, max, step, disabled } = this.props
     return (
       <div className='ling-slider-wrapper' ref={this.container}>
-        <div className='ling-slider-dot' style={isMove ? { left: left + 'px', transform: 'scale(1.5)' } : { left: left + 'px' }} onMouseDown={(e) => { this.handleMouseDown(e) }}></div>
+        <div className='ling-slider-dot' style={isMove && !disabled ? { left: left + 'px', transform: 'scale(1.5)' } : { left: left + 'px' }} onMouseDown={(e) => { this.handleMouseDown(e) }}></div>
         <div className='ling-slider-bar' style={{ width: left + 6 + 'px' }}></div>
         {
-          (max - min) / 200 * left + 6
+          Math.floor((max - min) / 200 * left + 6)
         }
       </div>
     )
@@ -55,11 +62,13 @@ export default class Slider extends React.Component {
 Slider.propTypes = {
   min: PropTypes.number,
   max: PropTypes.number,
-  value: PropTypes.number,
-  step: PropTypes.number
+  value: PropTypes.oneOfType([PropTypes.number, PropTypes.arrayOf(PropTypes.number)]),
+  step: PropTypes.number,
+  disabled: PropTypes.bool
 }
 Slider.defaultProps = {
-  min: -10,
+  min: 0,
   max: 500,
-  step: 1
+  step: 10,
+  disabled: false
 }
