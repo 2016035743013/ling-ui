3afeefce6808fbc12fa04c71a7270f961183569c
2016035743013 <2199397354@qq.com>
2020-10-16 00:01:38 +0800

    '完善switch组件'

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@
 // 组件库参考文章  https://juejin.im/post/6844903749912100871#heading-6
 
 import React from 'react';
-import {Button, Radio, RadioGroup, Input, Icon, InputNumber,Checkbox, Message, Tooltip, Alert, Slider, Switch } from './components/index'
+import { Button, Radio, RadioGroup, Input, Icon, InputNumber, Checkbox, Message, Tooltip, Alert, Slider, Switch } from './components/index'
 import './App.scss'
 class App extends React.Component {
   constructor(props) {
-    super(props) 
+    super(props)
     this.state = {
       value: '0',
       isSelect: false,
@@ -21,9 +21,7 @@ class App extends React.Component {
     })
   }
   onChange = (value) => {
-    this.setState({
-      inputValue: value
-    })
+    console.log(value)
   }
   showMessage = () => {
     Message.info({
@@ -32,11 +30,17 @@ class App extends React.Component {
       type: 'info'
     })
   }
-  render () {
+  render() {
     const { value, isSelect, inputValue } = this.state
     return (
       <div className="App">
-        <Switch disabled />
+        <Switch activeColor="#13ce66"
+          inactiveColor="#ff4949"
+          onChange={this.onChange}
+          value="1"
+          onValue="1"
+          offValue="2"
+        />
       </div>
     );
   }
--- src/components/switch/index.jsx
+++ src/components/switch/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import './index.scss'
 export default class Switch extends React.Component {
   constructor(props) {
@@ -7,11 +8,29 @@ export default class Switch extends React.Component {
       active: true
     }
   }
+  componentDidMount() {
+    const { value, onValue, offValue } = this.props
+    if (value || value === onValue) {
+      this.setState({
+        active: true
+      })
+    } else {
+      this.setState({
+        active: false
+      })
+    }
+  }
   // 按钮关闭和开启
-  toggle = () => {
-    const { disabled } = this.props
+  onChange = () => {
+    const { disabled, onValue, offValue, onChange } = this.props
+    const { active } = this.state
     if(disabled) {
       return
+    }
+    if (active) { // 关闭
+      onChange(offValue)
+    } else { // 开启
+      onChange(onValue)
     }
     this.setState((state) => {
       return {
@@ -21,12 +40,34 @@ export default class Switch extends React.Component {
   }
   render() {
     const { active } = this.state
-    const { disabled } = this.props
+    const { disabled, activeColor, inactiveColor } = this.props
     return(
-      <div className={['ling-switch-wrapper', active ? 'ling-switch-wrapper-active' : ''].join(' ').trim()} onClick={this.toggle}>
-        <div className={['ling-switch-block', active ? 'ling-switch-blockmove' : ''].join(' ').trim()}></div>
+      <div 
+        className={['ling-switch-wrapper', active ? 'ling-switch-wrapper-active' : ''].join(' ').trim()} onClick={this.onChange}
+        style={{background: !active ? inactiveColor ? inactiveColor : '#dcdfe6' : activeColor ? activeColor : '#1890ff'}}
+      >
+        <div 
+          className={['ling-switch-block', active ? 'ling-switch-blockmove' : ''].join(' ').trim()}
+        >
+        </div>
         { disabled && <div className='ling-switch-mark'></div> }
       </div>
     )
   }
+}
+
+Switch.propTypes = {
+  value: PropTypes.oneOfType([PropTypes.bool, PropTypes.number, PropTypes.string]),
+  disabled: PropTypes.bool,
+  activeColor: PropTypes.string,
+  inactiveColor: PropTypes.string,
+  onValue: PropTypes.oneOfType([PropTypes.bool, PropTypes.number, PropTypes.string]),
+  offValue: PropTypes.oneOfType([PropTypes.bool, PropTypes.number, PropTypes.string]),
+  onChange: PropTypes.func
+}
+
+Switch.defaultProps = {
+  value: false,
+  onValue: true,
+  offValue: false
 }
