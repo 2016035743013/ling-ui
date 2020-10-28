a78b0c74cf35ddf53865c1939edef882129c33bd
2016035743013 <2199397354@qq.com>
2020-10-29 00:16:00 +0800

    '完善文档'

--- site/docs/.rcpress/config.js
+++ site/docs/.rcpress/config.js
@@ -8,9 +8,20 @@ module.exports = {
       { text: 'External', link: 'https://google.com' }
     ],
     sidebar: {
-      '/foo/': ['' /* /foo/ */, 'one' /* /foo/one.html */, 'two' /* /foo/two.html */],
-
-      '/bar/': ['' /* /bar/ */, 'three' /* /bar/three.html */, 'four' /* /bar/four.html */]
+      '/foo/': [
+        // ['one', 'page one'] /* /foo/one.html 或者 /foo 将会被重定向到 /foo/one */,
+        // {'two'}, /* /foo/two.html */
+        {
+          title: 'Group 1',
+          collapsable: false,
+          children: [['one', 'group 1-1']]
+        },
+        {
+          title: 'group 2',
+          collapsable: false,
+          children: [['two', 'group 2-1']]
+        }
+      ]
     },
     // 是否启用回到顶部按钮
     showBackToTop: true
